862f955db52bbe5f16b1a32c5ef2ae4644dede06
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-11-06 23:42:10 +0100

    clean up getCategoryList method

--- src/Utils/CategoryTreeFrontPage.php
+++ src/Utils/CategoryTreeFrontPage.php
@@ -6,22 +6,31 @@ use App\Utils\AbstractClass\CategoryTreeAbstract;
 
 class CategoryTreeFrontPage extends CategoryTreeAbstract
 {
+
+    public $html_1 = '<ul>';
+    public $html_2 = '<li>';
+    public $html_3 = '<a href="';
+    public $html_4 = '">';
+    public $html_5 = '</a>';
+    public $html_6 = '</li>';
+    public $html_7 = '</ul>';
+    
     public function getCategoryList(array $categories_array)
     {
-        $this->categoryList .= '<ul>';
+        $this->categoryList .= $this->html_1;
         foreach ($categories_array as $value)
         {
             
             $catName = $value->getName();
             $url = $this->urlgenerator->generate('video_list', ['categoryname'=>$catName, 'id'=>$value->getId()]);
-            $this->categoryList .= '<li>' . '<a href="' . $url . '">' . $catName . '</a>';
+            $this->categoryList .= $this->html_2 . $this->html_3 .  $url . $this->html_4 . $catName . $this->html_5;
             if(!empty($value->children))
             {
                 $this->getCategoryList($value->children);
             }
-            
+            $this->categoryList .= $this->html_6;
         }
-        $this->categoryList .= '</ul>';
+        $this->categoryList .= $this->html_7;
         return $this->categoryList;
     }
 }
