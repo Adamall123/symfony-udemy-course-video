3d193537947be7a2e8126bf8fb8a15448c425dec
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-11-07 19:03:26 +0100

    add sort video by title

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use App\Entity\Video;
 use App\Utils\CategoryTreeFrontPage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,14 +22,13 @@ class FrontController extends AbstractController
     /**
      * @Route("/video-list/category/{categoryname},{id}/{page}", defaults={"page": "1"} , name="video_list")
      */
-    public function videoList($id, $page, CategoryTreeFrontPage $categories): Response
+    public function videoList($id, $page, CategoryTreeFrontPage $categories, Request $request): Response
     {
-        //$subcategories = $categories->buildTree($id);
-        //dump($subcategories); 
+        
         $categories->getCategoryListAndParent($id);
         $ids = $categories->getChildIds($id);
         array_push($ids, $id);
-        $videos = $this->getDoctrine()->getRepository(Video::class)->findByChildIds($ids ,$page);
+        $videos = $this->getDoctrine()->getRepository(Video::class)->findByChildIds($ids ,$page, $request->get('sortby'));
         // dump($categories->getCategoryListAndParent($id));
         // exit;
         return $this->render('front/video_list.html.twig',[
